b666932564c8fbe82f4e9ed6b59c6f0185aa2e0d
Silwings <1910506332@qq.com>
2022-01-19 23:25:41 +0800

    ${}同时支持简单json path查找和标准json path查找,优先返回标准path查找结果

--- src/main/java/com/silwings/responder/core/RequestParamsAndBody.java
+++ src/main/java/com/silwings/responder/core/RequestParamsAndBody.java
@@ -1,6 +1,8 @@
 package com.silwings.responder.core;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.JSONPath;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
@@ -9,6 +11,8 @@ import org.apache.commons.lang3.StringUtils;
 
 import java.util.Collections;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @ClassName RequestParamsAndBody
@@ -82,35 +86,70 @@ public class RequestParamsAndBody {
         }
 
         // 尝试从请求体中获取参数
-        if (!paramName.contains(".")) {
-            return this.getBody().get(paramName);
-        }
+        final Object simpleJsonPathResult = this.simpleJsonPathEval(this.getBody(), paramName);
+        final Object evalResult = JSONPath.eval(this.getBody(), paramName);
 
-        Object result = null;
-        final String[] paramArray = paramName.split("\\.");
-        if (paramArray.length == 1) {
-            result = this.getBody().get(paramArray[0]);
-        }
+        // 简单path和标准path同时查找,如果标准path有值,优先返回标准path结果
+        return null == evalResult ? simpleJsonPathResult : evalResult;
+    }
 
-        JSONObject jsonObject = null;
+    /**
+     * description: 通过自定义的简单Json Path 规则查找
+     * 规则
+     * 1.单层级直接指定key名
+     * 2.复数层级直接使用逗号连接
+     * 3.数组取值在key名后添加[],[]之间填写角标
+     * version: 1.0
+     * date: 2022/1/19 23:18
+     * author: Silwings
+     *
+     * @param jsonObject json对象
+     * @param simplePath 自定义path
+     * @return java.lang.Object 结果
+     */
+    private Object simpleJsonPathEval(final JSONObject jsonObject, final String simplePath) {
+
+        Object result = jsonObject;
+
         try {
+            final String[] paramArray = simplePath.split("\\.");
+
             for (int i = 0; i <= paramArray.length - 1; i++) {
-                // 当获取到最后一个元素时,使用Object接收,防止出现格式问题
-                if (i == paramArray.length - 1) {
-                    result = jsonObject.get(paramArray[i]);
-                } else if (null != jsonObject) {
-                    // 数组格式不应该出现在中间属性上,所以这里直接赋值给类型.
-                    jsonObject = jsonObject.getJSONObject(paramArray[i]);
+
+                if (null == result) {
+                    break;
+                }
+
+                if (this.isArrayIndex(paramArray[i])) {
+
+                    final String arrayKey = paramArray[i].replaceAll("\\[[0-9]+]", "");
+                    if (StringUtils.isNotBlank(arrayKey)) {
+                        // 如果指定了key,那么middleValue就必须是JSONObject类型,否则就应该退出循环,这里通过异常退出循环
+                        result = ((JSONObject) result).get(arrayKey);
+                    }
+
+                    // 如果middleValue不是JSONArray,通过异常退出循环
+                    final String indexStr = paramArray[i].replaceAll("\\S*\\[", "").replace("]", "");
+                    result = ((JSONArray) result).toArray()[Integer.parseInt(indexStr)];
+
                 } else {
-                    jsonObject = this.getBody().getJSONObject(paramArray[i]);
+
+                    // 如果middleValue不是JSONObject,通过异常退出循环
+                    result = ((JSONObject) result).get(paramArray[i]);
                 }
             }
         } catch (Exception e) {
-            log.error("尝试从请求体获取信息失败", e);
+            log.warn("使用简单JSON path搜索字段失败.失败原因: {}", e.getMessage());
         }
 
         return result;
     }
 
+    private static final Pattern ARRAY_INDEX_PATTERN = Pattern.compile("\\S*\\[[0-9]+]");
+
+    private boolean isArrayIndex(final String arrayIndex) {
+        final Matcher matcher = ARRAY_INDEX_PATTERN.matcher(arrayIndex);
+        return matcher.matches();
+    }
 
 }
